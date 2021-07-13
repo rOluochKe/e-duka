48888a6c7814da05e2db56b06042c6b12d05e1ac
Raymond Oluoch <rasholuoch@gmail.com>
2021-07-14 01:11:53 +0300

    project structure

--- src/App.js
+++ src/App.js
@@ -2,8 +2,46 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
+import {
+  Home,
+  About,
+  Products,
+  SingleProduct,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from './pages'
+
 const App = () => {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct />} />
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+        <Route exact path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route exact path='*'>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
