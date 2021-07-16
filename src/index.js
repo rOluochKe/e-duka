30b030d072db980c6cee9e49ed997d0c1dfa4bf9
Raymond Oluoch <rasholuoch@gmail.com>
2021-07-17 02:44:05 +0300

    fetching products and sorting

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ import { Auth0Provider } from '@auth0/auth0-react'
 ReactDOM.render(
   <React.StrictMode>
     <ProductsProvider>
-      <App />
+      <FilterProvider>
+        <App />
+      </FilterProvider>
     </ProductsProvider>
   </React.StrictMode>,
   document.getElementById('root')
